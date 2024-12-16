9dd8cef74f97b3c642a0b9b988cf5de8d625777c
Artyom <artyomkiselhof2007@gmail.com>
2024-12-16 13:54:32 +0200

    home

--- js/DarkTheme.js
+++ js/DarkTheme.js
@@ -1,15 +1,26 @@
 const sunIcon = document.querySelector(".bx-sun");
+const currentTheme = localStorage.getItem("theme");
 
-sunIcon.addEventListener("click", () => {
+if (currentTheme === "dark") {
+    document.body.classList.add("dark-theme");
+    sunIcon.classList.remove("bx-sun");
+    sunIcon.classList.add("bx-moon");
+} else {
+    document.body.classList.remove("dark-theme");
+    sunIcon.classList.remove("bx-moon");
+    sunIcon.classList.add("bx-sun");
+}
 
+sunIcon.parentElement.addEventListener("click", () => {
     if (sunIcon.classList.contains("bx-sun")) {
         sunIcon.classList.remove("bx-sun");
         sunIcon.classList.add("bx-moon");
+        document.body.classList.add("dark-theme");
+        localStorage.setItem("theme", "dark");
     } else {
         sunIcon.classList.remove("bx-moon");
         sunIcon.classList.add("bx-sun");
+        document.body.classList.remove("dark-theme");
+        localStorage.setItem("theme", "light");
     }
-
-    document.body.classList.toggle("dark-theme");
-
 });
