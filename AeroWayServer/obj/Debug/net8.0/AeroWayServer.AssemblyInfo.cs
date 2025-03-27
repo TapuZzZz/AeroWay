bd9bff1e0b2f94fe7c037afd184041b83a7f4232
TapuZzZz <Artyomkiselhof2007@gmail.com>
2025-03-27 20:11:19 +0200

    132

--- AeroWayServer/obj/Debug/net8.0/AeroWayServer.AssemblyInfo.cs
+++ AeroWayServer/obj/Debug/net8.0/AeroWayServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AeroWayServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+636f1a4c163f8750bddbfd2ee6dd29a333725329")]
 [assembly: System.Reflection.AssemblyProductAttribute("AeroWayServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AeroWayServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
